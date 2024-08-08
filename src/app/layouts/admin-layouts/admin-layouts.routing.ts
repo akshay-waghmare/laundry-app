2c629a591f2cf8005396b2153a8ab5be3d5568e8
paresh <pareshkumarahire@gmail.com>
2024-08-08 16:59:12 +0530

    redirect to Home page

--- src/app/layouts/admin-layouts/admin-layouts.routing.ts
+++ src/app/layouts/admin-layouts/admin-layouts.routing.ts
@@ -1,7 +1,8 @@
 import { AddCustomerComponent } from './../../add-customer/add-customer.component';
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
-import {DashboardComponent} from '../../dashboard/dashboard.component';
+import { HomeComponent } from 'src/app/home/home.component';
+//import {DashboardComponent} from '../../dashboard/dashboard.component';
 import {AddServiceComponent} from '../../add-service/add-service.component';
 import { ServiceListComponent } from 'src/app/service-list/service-list.component';
 import { CustomerListComponent } from '../../customer-list/customer-list.component';
@@ -12,7 +13,7 @@ import { AuthenticationGuard } from 'src/app/authentication.guard';
 import { BetMarketComponent } from 'src/app/bet-market/bet-market.component';
 import { TennisListComponent } from 'src/app/tennis-card-list/tennis-list/tennis-list.component';
 import { TennisRankingComponent } from 'src/app/tennis-card-list/tennis-list/tennis-ranking/tennis-ranking.component';
-import { HomeComponent } from 'src/app/home/home.component';
+
 
 import { CricketOddsComponent } from 'src/app/cricket-odds/cricket-odds.component';
 import { BetHistoryComponent } from 'src/app/bet-history/bet-history.component';
@@ -20,8 +21,10 @@ import { LogoutFormComponent } from 'src/app/logout-form/logout-form.component';
 import { ProfitLossComponent } from 'src/app/profit-loss/profit-loss.component';
 import { ScrapeControlComponent } from 'src/app/scrape-control/scrape-control.component';
 
+
 export const AdminLayoutsRoute: Routes = [
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthenticationGuard] },
+  { path: 'Home', component: HomeComponent, canActivate: [AuthenticationGuard] },
+  //{ path: 'dashboard', component: DashboardComponent, canActivate: [AuthenticationGuard] },
   { path: 'add-service', component: AddServiceComponent, canActivate: [AuthenticationGuard] },
   { path: 'football', component: ServiceListComponent, canActivate: [AuthenticationGuard] },
   { path: 'add-customer', component: AddCustomerComponent, canActivate: [AuthenticationGuard] },
@@ -37,7 +40,6 @@ export const AdminLayoutsRoute: Routes = [
   { path: 'account/profit-loss', component: ProfitLossComponent, canActivate: [AuthenticationGuard] },
   { path: 'scraping', component: ScrapeControlComponent, canActivate: [AuthenticationGuard] },
   { path: 'logout', component: LogoutFormComponent },
-  {path:'Home',component:HomeComponent},
 ];
 
 
